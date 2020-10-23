ff560142b7c445a8277830b38907869a23313ea4
Angelo Estoque <angelo.estoque@yahoo.com>
2020-10-24 00:07:12 +1100

    Simple ui and adjustment to ads
    Made a super basic ui that displays the health of everything and it only shows numbers also made an adjustment to the ads system which makes it zoom in a little when aiming down sight by changing its field of view to a smaller value

---
+++ Assets/Scripts/Enemy/HealthBarRotation.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthBarRotation : MonoBehaviour
+{
+    public Transform playerCam;
+
+    private void Start()
+    {
+        playerCam = GameObject.Find("Main Camera").transform;
+    }
+    private void LateUpdate()
+    {
+        transform.LookAt(transform.position + playerCam.forward);
+    }
+}
